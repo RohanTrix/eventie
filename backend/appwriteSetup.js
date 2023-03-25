bf179ee616495613c68904e970a304cfa531f937
Suven-p <pandeysuven@gmail.com>
2023-03-25 18:20:50 +0545

    Add register route

--- backend/appwriteSetup.js
+++ backend/appwriteSetup.js
@@ -1,11 +1,12 @@
-import { Account, Client, Databases } from 'appwrite';
+import {Client, Account, Databases} from 'node-appwrite';
 
 export default function(config) {
-    const {PORT, ENDPOINT, PROJECT} = config;
+    const {PORT, ENDPOINT, PROJECT, API_KEY} = config;
     const client = new Client();
     client
         .setEndpoint(ENDPOINT)
-        .setProject(PROJECT);
+        .setProject(PROJECT)
+        .setKey(API_KEY);
     const account = new Account(client);
     const databases = new Databases(client);
 
--- backend/routes/events/index.js
+++ backend/routes/events/index.js
@@ -22,4 +22,26 @@ router.post('/create', async (req, res) => {
 
 })
 
+router.post('/register', async (req, res) => {
+    const { userId, eventId } = req.body;
+    const {config, databases} = req.app.locals;
+
+    const eventData = await databases.getDocument(config.DATABASE_ID, config.COLLECTION_ID, eventId);
+    console.log(eventData);
+    const participants = [...eventData.participants, userId];
+    const data =  {
+        participants: [... new Set(participants)]
+    }
+    try {
+        const response = await databases.updateDocument(config.DATABASE_ID, config.COLLECTION_ID, eventId, JSON.stringify(data));
+        res.json(response)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Error while registering for event",
+            success: false,
+        })
+    }
+})
+
 export default router;
