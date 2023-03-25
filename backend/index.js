92d3d15d1b1b9b459acb3533ac362477307d9909
Suven-p <pandeysuven@gmail.com>
2023-03-25 22:09:15 +0545

    Add cors header

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const appwriteVariables = appwriteSetup(config);
 app.locals = {...app.locals, ...appwriteVariables, config};
 
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  next();
+});
 app.use('/api', apiRoute);
 
 app.listen(PORT, () => {
